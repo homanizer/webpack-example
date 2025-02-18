27fe77f6441317ccbb0e3abbec979c86ff4a1cf7
homanito <houman.mohebbi@gmail.com>
2025-02-18 15:55:49 +0100

    Upgrade Quill to 2.0.3 and add quill-table-up module

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import "quill/dist/quill.snow.css";
 
 import Quill from "quill/core";
-
+import TableUp, { defaultCustomSelect, TableAlign, TableMenuContextmenu, TableMenuSelect, TableResizeBox, TableResizeScale, TableSelection, TableVirtualScrollbar } from 'quill-table-up';
 import Toolbar from "quill/modules/toolbar";
 import Snow from "quill/themes/snow";
 
 import Bold from "quill/formats/bold";
 import Italic from "quill/formats/italic";
 import Header from "quill/formats/header";
+import 'quill/dist/quill.snow.css';
+import 'quill-table-up/index.css';
+import 'quill-table-up/table-creator.css';
+
+// Register the TableUp module
+Quill.register({ [`modules/${TableUp.moduleName}`]: TableUp }, true);
 
 Quill.register({
   "modules/toolbar": Toolbar,
@@ -15,10 +21,32 @@ Quill.register({
   "formats/bold": Bold,
   "formats/italic": Italic,
   "formats/header": Header,
+  "modules/quill-table-up": TableUp,
 });
 
-new Quill("#editor", {
-  theme: "snow",
+const quill = new Quill('#editor', {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'formula'],
+      [{ [TableUp.toolName]: [] }]
+    ],
+    [TableUp.moduleName]: {
+      scrollbar: TableVirtualScrollbar,
+      align: TableAlign,
+      resize: TableResizeBox,
+      resizeScale: TableResizeScale,
+      customSelect: defaultCustomSelect,
+      selection: TableSelection,
+      selectionOptions: {
+        tableMenu: TableMenuContextmenu,TableMenuSelect
+
+      }
+    },
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow', // or 'bubble'
 });
 
 export default Quill;
